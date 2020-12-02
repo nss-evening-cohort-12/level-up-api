b08c9d026d5d00906a417389580840e95fd72067
Hannah Hall <hannahkhall13@gmail.com>
2020-12-01 18:05:23 -0600

    add related_name

--- levelupapi/models/gamer_event.py
+++ levelupapi/models/gamer_event.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 
 class GamerEvent(models.Model):
-    gamer = models.ForeignKey("Gamer", on_delete=CASCADE)
-    event = models.ForeignKey("Event", on_delete=CASCADE)
+    gamer = models.ForeignKey("Gamer", on_delete=CASCADE, related_name="registrations")
+    event = models.ForeignKey("Event", on_delete=CASCADE, related_name="registrations")
